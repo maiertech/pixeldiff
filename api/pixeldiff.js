109f54a19bc7525424af91754aa71e13cb7ec590
Thilo Maier <git@maier.asia>
2020-02-18 23:11:33 -0500

    Rewrite lambda with express GH-20

--- api/pixeldiff.js
+++ api/pixeldiff.js
@@ -1,83 +1,52 @@
 const querystring = require('querystring');
 
-const validator = require('validator');
+const express = require('express');
+const { check, validationResult } = require('express-validator');
 const { devices } = require('puppeteer-core');
 const axios = require('axios');
 const pixelmatch = require('pixelmatch');
 const PNG = require('pngjs').PNG;
 
-module.exports = async (request, response) => {
-  let url;
-  let benchmarkImgUrl;
-  let device;
+const app = express();
 
-  switch (request.method) {
-    case 'GET':
-      url = request.query.url || '';
-      benchmarkImgUrl = request.query.benchmarkImgUrl || '';
-      device = request.query.device || '';
-      break;
-    case 'POST':
-      url = request.body.url || '';
-      benchmarkImgUrl = request.body.benchmarkImgUrl || '';
-      device = request.body.device || '';
-      break;
-    default:
-      // This branch should never be executed.
-      // Routes in now.json need to be configured to accept GET and POST only.
-      response.status(400);
-      response.json({
-        message: `${request.method} method not supported.`,
-      });
-      return;
+app.use(express.json());
+
+// Validations.
+const validations = [
+  check('url')
+    .isURL({
+      protocols: ['http', 'https'],
+      require_protocol: true,
+    })
+    .withMessage('Invalid URL'),
+  check('benchmarkImgUrl')
+    .isURL({
+      protocols: ['http', 'https'],
+      require_protocol: true,
+    })
+    .withMessage('Invalid URL'),
+  check('device')
+    .custom(device => devices[device])
+    .withMessage('Invalid device'),
+];
+
+// Restrict methods to GET and POST on Zeit Now.
+app.all('*', validations, async (request, response, next) => {
+  // Validate params.
+  const errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    return response.status(422).json({ errors: errors.array() });
   }
 
-  // Validate request parameters.
-  const validations = [
-    {
-      param: 'url',
-      value: url,
-      message: 'Invalid URL.',
-      validate: () =>
-        validator.isURL(url, {
-          protocols: ['http', 'https'],
-          require_protocol: true,
-        }),
-    },
-    {
-      param: 'benchmarkImgUrl',
-      value: benchmarkImgUrl,
-      message: 'Invalid URL.',
-      validate: () =>
-        validator.isURL(benchmarkImgUrl, {
-          protocols: ['http', 'https'],
-          require_protocol: true,
-        }),
-    },
-    {
-      param: 'device',
-      value: device,
-      message: 'Invalid device.',
-      validate: () => devices[device],
-    },
-  ];
+  // Read params.
+  const url = request.body.url || request.query.url;
+  const benchmarkImgUrl =
+    request.body.benchmarkImgUrl || request.query.benchmarkImgUrl;
+  const device = request.body.device || request.query.device;
 
-  // Every failed validation is converted into an error message.
-  const errors = validations
-    .filter(validation => !validation.validate())
-    .map(({ param, value, message }) => ({ param, value, message }));
-  if (errors.length !== 0) {
-    response.status(400);
-    response.json(errors);
-    return;
-  }
-
-  // Capture screenshot.
-  // Must use try catch syntax instead of adding .catch().
-  // Otherwise it is not possible to exit from lambda.
-  let captureResponse;
-  try {
-    captureResponse = await axios.post(
+  // Step 1: capture screenshot.
+  const captureResponse = await axios
+    .post(
       'https://capture.maier.tech/api',
       {
         url,
@@ -86,77 +55,42 @@ module.exports = async (request, response) => {
       {
         responseType: 'arraybuffer',
       }
+    )
+    .catch(error =>
+      next(new Error(`Failed to capture screenshot: ${error.message}`))
     );
-  } catch (error) {
-    // Return status 400 because url was probably bad.
-    response.status(400);
-    response.json([
-      {
-        param: 'url',
-        value: url,
-        message: `Failed to capture screenshot: ${error.message}`,
-      },
-    ]);
-    return;
-  }
 
+  // Step 2: read screenshot image as PNG.
   let screenshotImg;
   try {
     screenshotImg = PNG.sync.read(captureResponse.data);
   } catch (error) {
-    // Return status 500 because if something fails here we cannot blame the params.
-    response.status(500);
-    response.json([
-      {
-        param: 'url',
-        value: url,
-        message: `Failed to read screenshot image: ${error.message}`,
-      },
-    ]);
-    return;
+    return next(new Error(`Failed to read screenshot image: ${error.message}`));
   }
 
-  // Download benchmark image.
-  // Must use try catch syntax instead of adding .catch().
-  // Otherwise it is not possible to exit from lambda.
-  let downloadResponse;
-  try {
-    downloadResponse = await axios.get(benchmarkImgUrl, {
+  // Step 3: download benchmark image.
+  const downloadResponse = await axios
+    .get(benchmarkImgUrl, {
       responseType: 'arraybuffer',
-    });
-  } catch (error) {
-    // Return status 400 because benchmarkImgUrl was probably bad.
-    response.status(400);
-    response.json([
-      {
-        param: 'benchmarkImgUrl',
-        value: benchmarkImgUrl,
-        message: `Failed to download benchmark image: ${error.message}`,
-      },
-    ]);
-    return;
-  }
+    })
+    .catch(error =>
+      next(new Error(`Failed to download benchmark image: ${error.message}`))
+    );
 
+  // Step 4: read benchmark image as PNG.
   let benchmarkImg;
   try {
     benchmarkImg = PNG.sync.read(downloadResponse.data);
   } catch (error) {
-    // Return status 400 because download was probably not a valid PNG file.
-    response.status(400);
-    response.json([
-      {
-        param: 'benchmarkImgUrl',
-        value: benchmarkImgUrl,
-        message: `Failed to read benchmark image: ${error.message}`,
-      },
-    ]);
-    return;
+    return next(new Error(`Failed to read benchmark image: ${error.message}`));
   }
+
+  // Step 5: compute diff between screenshot image and benchmark image.
 
   // Default: do not create diff image.
   let diffImg = { data: null };
 
-  // GET request returns diff image.
+  // Only for GET request return diff image.
   if (request.method === 'GET') {
     // Diff image needs to have same size as benchmark image.
     diffImg = new PNG({
@@ -180,27 +114,23 @@ module.exports = async (request, response) => {
       }
     );
   } catch (error) {
-    // Return status 500 because if comparison erros out, we probably cannot blame params.
-    response.status(500);
-    response.send([
-      {
-        message: `Failed to compare screenshot with benchmark image: ${error.message}`,
-      },
-    ]);
-    return;
+    return next(
+      new Error(
+        `Failed to compare screenshot image with benchmark image: ${error.message}`
+      )
+    );
   }
 
   // For GET requests return diff image.
   if (request.method === 'GET') {
     response.setHeader('Content-Type', 'image/png');
     response.status(200);
-    response.send(PNG.sync.write(diffImg));
-    return;
+    return response.send(PNG.sync.write(diffImg));
   }
 
   // For POST requests return comparison result (diff) and URL to retrieve diff image (diffImgUrl).
   response.status(200);
-  response.json({
+  return response.json({
     diff: mismatchedPixels > 0,
     diffImgUrl: `https://pixeldiff.maier.tech/api?${querystring.stringify({
       url,
@@ -208,4 +138,6 @@ module.exports = async (request, response) => {
       device,
     })}`,
   });
-};
+});
+
+module.exports = app;
